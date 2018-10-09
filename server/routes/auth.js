f0b6c06387076a4b7f83de5030a80042b30d4ace
LauraMiguelanez <laura.miguelanez@gmail.com>
2018-10-09 16:08:14 +0200

    THREE in component

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const passport = require('passport');
+const passport = require("passport");
 const router = express.Router();
 const User = require("../models/User");
 
@@ -7,57 +7,84 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
 
-
-router.get("/login", (req, res, next) => {
-  res.render("auth/login", { "message": req.flash("error") });
+/* router.get("/login", (req, res, next) => {
+  res.render("auth/login", { message: req.flash("error") });
 });
+ */
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/auth/login",
+    failureFlash: true,
+    passReqToCallback: true
+  })
+);
 
-router.post("/login", passport.authenticate("local", {
-  successRedirect: "/",
-  failureRedirect: "/auth/login",
-  failureFlash: true,
-  passReqToCallback: true
-}));
-
-router.get("/signup", (req, res, next) => {
+/* router.get("/signup", (req, res, next) => {
   res.render("auth/signup");
-});
+}); */
 
 router.post("/signup", (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
-  if (username === "" || password === "") {
-    res.render("auth/signup", { message: "Indicate username and password" });
+
+  if (!username || !password) {
+    res.status(400).json({ message: "Provide username and password" });
+    return;
+  }
+  if (password.length < 7) {
+    res
+      .status(400)
+      .json({
+        message:
+          "Please make your password at least 8 characters long for security purposes."
+      });
     return;
   }
 
-  User.findOne({ username }, "username", (err, user) => {
-    if (user !== null) {
-      res.render("auth/signup", { message: "The username already exists" });
+  User.findOne({ username }, (err, foundUser) => {
+    if (err) {
+      res.status(500).json({ message: "Username check went bad." });
       return;
     }
-
-    const salt = bcrypt.genSaltSync(bcryptSalt);
+    if (foundUser) {
+      res.status(400).json({ message: "Username taken. Choose another one." });
+      return;
+    }
+    const salt = bcrypt.genSaltSync(10);
     const hashPass = bcrypt.hashSync(password, salt);
 
-    const newUser = new User({
-      username,
+    const aNewUser = new User({
+      username: username,
       password: hashPass
     });
 
-    newUser.save()
-    .then(() => {
-      res.redirect("/");
-    })
-    .catch(err => {
-      res.render("auth/signup", { message: "Something went wrong" });
-    })
+    aNewUser.save(err => {
+      if (err) {
+        res
+          .status(400)
+          .json({ message: "Saving user to database went wrong." });
+        return;
+      }
+      // Automatically log in user after sign up
+      // .login() here is actually predefined passport method
+      req.login(aNewUser, err => {
+        if (err) {
+          res.status(500).json({ message: "Login after signup went bad." });
+          return;
+        }
+        // Send the user's information to the frontend
+        // We can use also: res.status(200).json(req.user);
+        res.status(200).json(aNewUser);
+      });
+    });
   });
 });
 
-router.get("/logout", (req, res) => {
+/* router.get("/logout", (req, res) => {
   req.logout();
   res.redirect("/");
-});
+}); */
 
 module.exports = router;
