1fd4c8f8678beba5604923dc4a11461af6bc0ea5
LauraMiguelanez <laura.miguelanez@gmail.com>
2018-10-17 10:08:16 +0200

    Push to heroku

---
+++ server/public/precache-manifest.0e436733e50f2e36abe833e1823e44a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9c9cdf5aad0e0bff5f37",
+    "url": "/static/js/main.9c9cdf5a.chunk.js"
+  },
+  {
+    "revision": "267ffc69a4f5b9b773df",
+    "url": "/static/js/1.267ffc69.chunk.js"
+  },
+  {
+    "revision": "9c9cdf5aad0e0bff5f37",
+    "url": "/static/css/main.11ce27ff.chunk.css"
+  },
+  {
+    "revision": "267ffc69a4f5b9b773df",
+    "url": "/static/css/1.393305e3.chunk.css"
+  },
+  {
+    "revision": "821e7a418ce1048dbb129897d846e926",
+    "url": "/index.html"
+  }
+];
